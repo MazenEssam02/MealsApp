9409233ccb924f1f315bde2bab9590035cc26454
Mazen <Mazenessam2011@outlook.com>
2024-09-13 02:13:59 +0300

    finish favorite Screen

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,9 +1,33 @@
-import { StyleSheet,View,Text} from "react-native";
+import { useContext } from "react";
+import { StyleSheet,View,FlatList} from "react-native";
+import { FavoriteContext } from "../store/context/favorite-context";
+import MealItem from "../components/MealItem";
+import { MEALS } from "../data/dummy-data";
+
 function FavoriteScreens({navigation}){
-
+  const favoriteMealsCtx=useContext(FavoriteContext);
+  const displayedMeals=MEALS.filter((mealItem)=>{
+    return (favoriteMealsCtx.ids.includes(mealItem.id));
+  });
+  
+  function gridMealItem(itemData){
+    function pressHandler(){
+      navigation.navigate('MealDetails',{id:itemData.item.id});
+    }
+    return(
+      <MealItem item={itemData.item} onPress={pressHandler}/>
+    );
+   }
   return(
-<Text>Favorites</Text>
-
-   );
-}
+    <View style={styles.container}>
+     <FlatList data={displayedMeals} keyExtractor={(item)=>item.id} renderItem={gridMealItem} />
+    </View>
+  );
+  }
 export default FavoriteScreens;
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    
+  }
+});
--- screens/MealsScreenView.js
+++ screens/MealsScreenView.js
@@ -27,6 +27,6 @@ export default MealsScreenView;
 const styles=StyleSheet.create({
   container:{
     flex:1,
-    padding:16
+   
   }
 });
